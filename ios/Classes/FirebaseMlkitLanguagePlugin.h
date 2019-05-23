e16ce2f81bde6a0de3bda0da1ad98f2e9fc792e3
rishab2113 <rishab@bu.edu>
2019-05-23 18:29:15 -0400

    xcode skeleton

--- ios/Classes/FirebaseMlkitLanguagePlugin.h
+++ ios/Classes/FirebaseMlkitLanguagePlugin.h
@@ -5,3 +5,17 @@
 @interface FLTFirebaseMlkitLanguagePlugin : NSObject<FlutterPlugin>
 + (void)handleError:(NSError *)error result:(FlutterResult)result;
 @end
+
+@protocol LangAgent
+@required
++ (void)handleEvent:(NSString *)text
+            options:(NSDictionary *)options
+             result:(FlutterResult)result;
+@optional
+@end
+
+@interface LanguageIdentifier : NSObject <LangAgent>
+@end
+
+@interface LanguageTranslator : NSObject <LangAgent>
+@end
