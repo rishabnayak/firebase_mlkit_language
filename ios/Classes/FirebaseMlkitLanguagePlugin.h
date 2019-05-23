b0d3c9d127156b5504e3ab86191a7d835dc1c2e0
rishab2113 <rishab@bu.edu>
2019-05-23 10:28:37 -0400

    Initial commit

---
+++ ios/Classes/FirebaseMlkitLanguagePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FirebaseMlkitLanguagePlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FirebaseMlkitLanguagePlugin.m
@@ -0,0 +1,20 @@
+#import "FirebaseMlkitLanguagePlugin.h"
+
+@implementation FirebaseMlkitLanguagePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"firebase_mlkit_language"
+            binaryMessenger:[registrar messenger]];
+  FirebaseMlkitLanguagePlugin* instance = [[FirebaseMlkitLanguagePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
