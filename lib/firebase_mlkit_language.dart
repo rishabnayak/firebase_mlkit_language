58c0cea4b2795c5a0bf03e3253a6e8fa40361dbc
Nikzad Khani <khaninikzad@gmail.com>
2019-05-24 03:51:32 -0400

    Model Manager

--- lib/firebase_mlkit_language.dart
+++ lib/firebase_mlkit_language.dart
@@ -1,9 +1,6 @@
 library firebase_mllanguage;
 
 import 'dart:async';
-// import 'dart:io';
-// import 'dart:typed_data';
-// import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
@@ -11,3 +8,4 @@ import 'package:flutter/services.dart';
 part 'src/firebase_language.dart';
 part 'src/language_identifier.dart';
 part 'src/language_translator.dart';
+part 'src/model_manager.dart';
