a2e7ce9a6859e9df8da1f0abdca57c47ccac64eb
rishab2113 <rishab@bu.edu>
2019-05-25 12:51:05 -0400

    updates

--- ios/Classes/DownloadModel.m
+++ ios/Classes/DownloadModel.m
@@ -5,19 +5,19 @@
 @implementation DownloadModel
 
 + (void)handleEvent:(NSString *)text result:(FlutterResult)result {
-    FIRTranslateLanguage modelName = FIRTranslateLanguageForLanguageCode(text);
-    FIRModelDownloadConditions *conditions =
-    [[FIRModelDownloadConditions alloc] initWithAllowsCellularAccess:YES
-                                         allowsBackgroundDownloading:YES];
-    FIRTranslateRemoteModel *modelToDownload =
-    [FIRTranslateRemoteModel translateRemoteModelForApp:FIRApp.defaultApp
-                                               language:modelName
-                                             conditions:conditions];
-    if ([[FIRModelManager modelManager] isRemoteModelDownloaded:modelToDownload]) {
-        result(@"Already Downloaded");
-    } else{
-        [[FIRModelManager modelManager] downloadRemoteModel:modelToDownload];
-        result(@"Downloaded");
-    }
+  FIRTranslateLanguage modelName = FIRTranslateLanguageForLanguageCode(text);
+  FIRModelDownloadConditions *conditions =
+      [[FIRModelDownloadConditions alloc] initWithAllowsCellularAccess:YES
+                                           allowsBackgroundDownloading:YES];
+  FIRTranslateRemoteModel *modelToDownload =
+      [FIRTranslateRemoteModel translateRemoteModelForApp:FIRApp.defaultApp
+                                                 language:modelName
+                                               conditions:conditions];
+  if ([[FIRModelManager modelManager] isRemoteModelDownloaded:modelToDownload]) {
+    result(@"Already Downloaded");
+  } else {
+    [[FIRModelManager modelManager] downloadRemoteModel:modelToDownload];
+    result(@"Downloaded");
+  }
 }
 @end
--- ios/Classes/ViewModels.m
+++ ios/Classes/ViewModels.m
@@ -4,21 +4,19 @@
 
 @implementation ViewModels
 
-
 + (void)result:(FlutterResult)result {
-    NSSet<FIRTranslateRemoteModel *> *localModels =
-    [[FIRModelManager modelManager] availableTranslateModelsWithApp:FIRApp.defaultApp];
-    
-    NSArray *array = localModels.allObjects;
-    
-    NSMutableArray *arr = [NSMutableArray array];
-    for (FIRTranslateRemoteModel *model in array) {
-        NSString *languageCode = FIRTranslateLanguageCodeForLanguage(model.language);
-        [arr addObject: languageCode];
-    }
-    
-    result(arr);
-}
+  NSSet<FIRTranslateRemoteModel *> *localModels =
+      [[FIRModelManager modelManager] availableTranslateModelsWithApp:FIRApp.defaultApp];
 
+  NSArray *array = localModels.allObjects;
+
+  NSMutableArray *arr = [NSMutableArray array];
+  for (FIRTranslateRemoteModel *model in array) {
+    NSString *languageCode = FIRTranslateLanguageCodeForLanguage(model.language);
+    [arr addObject:languageCode];
+  }
+
+  result(arr);
+}
 
 @end
